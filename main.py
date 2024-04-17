8f399c151f6de3214abdf132adce27610bc13ccf
ICawesome <162729625+ICawesome@users.noreply.github.com>
2024-04-17 16:18:36 +0000

    FC stands for Full Combo!

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+temp = int(input("enter the temperature:\n\n"))
+choice = input("\nenter 1 for Fahrenheit to Celcius. \nenter 2 for Celcius to fahrenheit.\n\n")
+if choice == "1":
+    # the flowchart says "no" in both directions lol
+    print("\nthe temperature in Celcius is: " + str((5/9)*(temp-32)))
+elif choice == "2":
+    print("\nthe temperature in fahrenheit is: " + str((9*temp+(32*5))/5))
+else:
+    print("\ninvalid input")
